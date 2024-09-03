4cb2d47282ba16c0cc1560e298ec40340d551f3c
Haozhe Zhang <zhz19980514@gmail.com>
2024-09-03 18:04:35 -0500

    feat: enable kafka for local environment

--- src/main/java/com/cognitube/consumer/config/ConsumerConfig.java
+++ src/main/java/com/cognitube/consumer/config/ConsumerConfig.java
@@ -139,10 +139,6 @@ public class ConsumerConfig {
             @Value("${kafka.password}") String password,
             @Value("${kafka.bootstrap-servers}") String bootstrapServers
     ) {
-        System.out.println("Bootstrap Servers: " + bootstrapServers);
-        System.out.println("Namespace: " + namespace);
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
         return new KafkaConsumerConfig(
                 namespace,
                 username,
@@ -158,11 +154,6 @@ public class ConsumerConfig {
             @Value("${kafka.password}") String password,
             @Value("${kafka.bootstrap-servers}") String bootstrapServers
     ) {
-        System.out.println("Bootstrap Servers: " + bootstrapServers);
-        System.out.println("Namespace: " + namespace);
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
-
         return new KafkaProducerConfig(
                 namespace,
                 username,
